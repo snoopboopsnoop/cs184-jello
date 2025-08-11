50bdd40c2137665ce0a131ec005b2dfc7b56504c
cjchengscu <cjcheng@scu.edu>
2025-08-11 03:10:47 -0700

    workinggit status

--- src/main.cpp
+++ src/main.cpp
@@ -211,7 +211,7 @@ int main() {
 		processInput(window); // handle inputs
 
 		//render
-		glClearColor(1.0f, 0.1f, 0.1f, 1.0f);
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //wireframe mode
